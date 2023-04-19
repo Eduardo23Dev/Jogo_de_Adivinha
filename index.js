283ebf3938e250903ff7f7fca663854e260a7409
Eduardo Barroso <119080987+Eduardo23Dev@users.noreply.github.com>
2023-04-18 22:09:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const myButton = document.getElementById("myButton");
 const randomNumber = Math.floor(Math.random() * 11);
+const allowedKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let cont = 0;
+let inputTest = document.getElementById("myInput");
+inputTest.addEventListener("keydown", (ev) => {
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    inputTest.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    inputTest.value = inputTest.value.slice(0, -1);
+  }
+});
 myButton.addEventListener("click", (ev) => {
-  ev.preventDefault;
+  ev.preventDefault();
   cont++;
   let input = document.getElementById("myInput");
   if (input.value === randomNumber.toString()) {
@@ -15,10 +27,13 @@ myButton.addEventListener("click", (ev) => {
   } else if (input.value > 10 || input.value < 0) {
     alert("Voce so pode escolher numeros entre 0 e 10!");
     input.value = "";
+    input.focus();
   } else if (input.value === "") {
     alert("Voce precisa digitar algum numero entre 0 e 10!");
+    input.focus();
   } else {
     alert("Voce errou!");
     input.value = "";
+    input.focus();
   }
 });
